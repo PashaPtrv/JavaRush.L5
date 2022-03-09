0aad3d3fdab748a6128fc23946e81152fd70f62a
Pasha <pvlptrv@yandex.ru>
2022-03-09 03:44:48 +0300

    Лекция 5, блок 1, задание 4-5.

--- src/Cat.java
+++ src/Cat.java
@@ -1,45 +1,39 @@
 public class Cat {
 
-    //Создать класс Cat.
-    // У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
-    public String name;
-    public int age;
-    public int weight;
-    public int strength;
+    //В методе main создать три объекта типа Cat и заполнить их данными.
+    // Использовать класс Cat из первой задачи. Класс Cat создавать не надо.
+    public static void main(String[] args){
+        Cat cat1 = new Cat("Boris", 7,7,5);
+        Cat cat2 = new Cat("Bublik", 4,5,6);
+        Cat cat3 = new Cat("Barsik", 2,3,4);
 
+        //Создать три кота используя класс Cat.
+        //Провести три боя попарно между котами.
+        //Класс Cat создавать не надо. Для боя использовать метод boolean fight(Cat anotherCat).
+        //Результат каждого боя вывести на экран.
 
-    /* Реализовать метод boolean fight(Cat anotherCat):
-     реализовать механизм драки котов в зависимости от их веса, возраста и силы.
-      Зависимость придумать самому. Метод должен определять, выиграли ли мы (this) бой или нет,
-       т.е. возвращать true, если выиграли и false - если нет.
-        Должно выполняться условие:
-        если cat1.fight(cat2) == true , то cat2.fight(cat1) == false */
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat2.fight(cat3));
+        System.out.println(cat3.fight(cat1));
+    }
 
+    private String name;
+    private int age;
+    private int weight;
+    private int strength;
 
+    public Cat(String name, int age, int weight, int strength) {
+
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.strength = strength;
+    }
     public boolean fight(Cat anotherCat){
 
         if (this.age + this.strength*this.weight > anotherCat.age + anotherCat.strength * anotherCat.weight)
             return true;
-            else return false;
-
-    }
-
-
-    public static void main(String[] args){
-        Cat Boris = new Cat();
-        Boris.name = "Борис";
-        Boris.age = 7;
-        Boris.weight = 6;
-        Boris.strength = 4;
-
-        Cat Bublik = new Cat();
-        Bublik.name = "Бублик";
-        Bublik.age = 5;
-        Bublik.weight = 4;
-        Bublik.strength = 7;
-
-        System.out.println(Bublik.fight(Boris));
-
+        else return false;
 
     }
 }
