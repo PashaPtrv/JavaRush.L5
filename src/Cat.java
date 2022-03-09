aea9c4ca453fa47d062dff2d0e24a256761d3bf2
Pasha <pvlptrv@yandex.ru>
2022-03-09 05:18:05 +0300

    Лекция 5, блок 2, задание 2.

--- src/Cat.java
+++ src/Cat.java
@@ -1,39 +1,47 @@
 public class Cat {
 
-    //В методе main создать три объекта типа Cat и заполнить их данными.
-    // Использовать класс Cat из первой задачи. Класс Cat создавать не надо.
-    public static void main(String[] args){
-        Cat cat1 = new Cat("Boris", 7,7,5);
-        Cat cat2 = new Cat("Bublik", 4,5,6);
-        Cat cat3 = new Cat("Barsik", 2,3,4);
-
-        //Создать три кота используя класс Cat.
-        //Провести три боя попарно между котами.
-        //Класс Cat создавать не надо. Для боя использовать метод boolean fight(Cat anotherCat).
-        //Результат каждого боя вывести на экран.
-
-        System.out.println(cat1.fight(cat2));
-        System.out.println(cat2.fight(cat3));
-        System.out.println(cat3.fight(cat1));
-    }
+    /*Создать класс Cat (кот) с пятью инициализаторами:
+- Имя
+- Имя, вес, возраст
+- Имя, возраст (вес стандартный)
+- вес, цвет, (имя, адрес и возраст неизвестны, это бездомный кот)
+- вес, цвет, адрес (чужой домашний кот)
+Задача инициализатора – сделать объект валидным.
+ Например, если вес неизвестен, то нужно указать какой-нибудь средний вес.
+ Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null).
+  То же касается адреса: null.*/
 
     private String name;
-    private int age;
-    private int weight;
-    private int strength;
+    private String address ;
+    private int age ;
+    private int weight ;
+    private String color;
 
-    public Cat(String name, int age, int weight, int strength) {
-
+    public void initialize(String name){
+        this.name = name;
+    }
+    public void initialize(String name,int age, int weight){
         this.name = name;
         this.age = age;
         this.weight = weight;
-        this.strength = strength;
     }
-    public boolean fight(Cat anotherCat){
-
-        if (this.age + this.strength*this.weight > anotherCat.age + anotherCat.strength * anotherCat.weight)
-            return true;
-        else return false;
+    public void initialize(String name,int age){
+        this.name = name;
+        this.age = age;
+        this.weight = 5;
 
     }
+    public void initialize(int weight, String color){
+        this.weight = weight;
+        this.color = color;
+        this.name = null;
+        this.address = null;
+        this.age = 8;
+    }
+    public void initialize(int weight, String color, String address){
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+    }
+
 }
