5a11254c8f2c172d47d87d951f8ef0e9d8ead7cd
Pasha <pvlptrv@yandex.ru>
2022-03-09 05:56:57 +0300

    Лекция 5, блок 3, задание 5.

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,27 +1,37 @@
+//Создать класс прямоугольник (Rectangle). Его данными будут top, left, width, height (левая координата, верхняя, ширина и высота).
+// Создать для него как можно больше конструкторов:
+//
+//Примеры:
+//- заданы 4 параметра: left, top, width, height
+//- ширина/высота не задана (оба равны 0)
+//- высота не задана (равно ширине) создаём квадрат
+//- создаём копию другого прямоугольника (он и передаётся в параметрах)
+
+
 public class Rectangle {
 
     private int left, top, width, height;
 
-    public void initialize(int left, int top, int width, int height) {
+    public Rectangle(int left, int top, int width, int height) {
         this.left = left;
         this.top = top;
         this.width = width;
         this.height = height;
     }
 
-    public void initialize(int left, int top) {
+    public Rectangle(int left, int top) {
         this.left = left;
         this.top = top;
     }
 
-    public void initialize(int left, int top, int width) {
+    public Rectangle(int left, int top, int width) {
         this.left = left;
         this.top = top;
         this.width = width;
         height = width;
     }
 
-    public void initialize(Rectangle anotherRectangle) {
+    public Rectangle(Rectangle anotherRectangle) {
         this.left = anotherRectangle.left;
         this.top = anotherRectangle.top;
         this.width = anotherRectangle.width;
