c2ca1508fcf8e9f645ac8fe2e25910712b8c2109
Pasha <pvlptrv@yandex.ru>
2022-03-09 05:48:14 +0300

    Лекция 5, блок 3, задание 2.

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,6 @@
 public class Cat {
 
-    /*Создать класс Cat (кот) с пятью инициализаторами:
+    /*Создать класс Cat (кот) с пятью конструкторами:
 - Имя
 - Имя, вес, возраст
 - Имя, возраст (вес стандартный)
@@ -11,34 +11,35 @@ public class Cat {
  Кот не может ничего не весить. То же касательно возраста. А вот имени может и не быть (null).
   То же касается адреса: null.*/
 
+    //
     private String name;
     private String address ;
     private int age ;
     private int weight ;
     private String color;
 
-    public void initialize(String name){
+    public Cat(String name){
         this.name = name;
     }
-    public void initialize(String name,int age, int weight){
+    public Cat(String name,int age, int weight){
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
-    public void initialize(String name,int age){
+    public Cat(String name,int age){
         this.name = name;
         this.age = age;
         this.weight = 5;
 
     }
-    public void initialize(int weight, String color){
+    public Cat(int weight, String color){
         this.weight = weight;
         this.color = color;
         this.name = null;
         this.address = null;
         this.age = 8;
     }
-    public void initialize(int weight, String color, String address){
+    public Cat(int weight, String color, String address){
         this.weight = weight;
         this.color = color;
         this.address = address;
